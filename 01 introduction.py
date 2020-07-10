f2f0d500a1cc10ee6dea602fb7b5bb7a8e4f4cf7
Jesper Vang <jvang@jespers-MacBook-Pro.local>
2020-07-10 18:41:10 +0200

    ❤️learning stuff

--- 01 introduction.py
+++ 01 introduction.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
  
-###########################################################
-#      __                     _____                       #
-#   __|  |___ ___ ___ ___ ___|  |  |                      #
-#  |  |  | -_|_ -| . | -_|  _|  |  |                      #
-#  |_____|___|___|  _|___|_|  \___/                       #
-#                |_|                                      #
-# encoding: utf                                           #
-# Copyright (c) 2020 Jesper Vang <jesper_vang@me.com>     #
-# MIT Licence. See http://opensource.org/licenses/MIT     #
-# Created on 4 Jul 2020                                       #
-###########################################################
+##########################################################
+# encoding: utf                                          #
+# Copyright (c) Jesper Vang <jesper_vang@me.com>         #
+# MIT Licence. See http://opensource.org/licenses/MIT    #
+# Created on 5 Jul 2020                                  #
+##########################################################
+
 
 """
 This is code for the introduction chapter. As such, it stands alone
@@ -31,13 +27,16 @@ users = [
     { "id": 9, "name": "Klein" }
 ]
 
+# list of pairs 
 friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                     (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
-# Initialize the dict with an empty list for each user id:
+#  for each user id:
 friendships = {user["id"]: [] for user in users}
+print(friendships)
 
 # And loop over the friendship pairs to populate it:
 for i, j in friendship_pairs:
     friendships[i].append(j)  # Add j as a friend of user i
     friendships[j].append(i)  # Add i as a friend of user j
+
